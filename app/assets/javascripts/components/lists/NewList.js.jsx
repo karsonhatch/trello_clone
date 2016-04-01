35c0a68af8b79f202054ffe4717e047f01d9358d
Karson Hatch <karsonhatch@gmail.com>
2016-04-01 09:35:32 -0600

    initial

---
+++ app/assets/javascripts/components/lists/NewList.js.jsx
@@ -0,0 +1,35 @@
+class NewList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.addList = this.addList.bind(this);
+  }
+
+  addList(e) {
+    let name = this.refs.name;
+    e.preventDefault();
+    $.ajax({
+      url: `/boards/${this.props.id}/lists`,
+      type: 'POST',
+      data: { list: { name: name.value }},
+      dataType: 'JSON'
+    }).success( list => {
+      this.props.addList(list);
+    }).error( errors => {
+      alert(errors);
+    }).complete( () => {
+      name.value = null;
+    });
+  }
+
+  render() {
+    return(
+      <div className="col s12 m10 offset-m1">
+        <h4>Add a new list</h4>
+        <form onSubmit={this.addList}>
+          <input placeholder="Name" ref="name" required={true} />
+          <button className="btn">Add</button>
+        </form>
+      </div>
+    )
+  }
+}
